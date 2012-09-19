e8f67d8438e3e14028c909e37096c9e8c9b773e9
Lucas dos Santos <santoslucasls@gmail.com>
2012-09-19 00:10:01 -0300

    criei a aplicacao rails

---
+++ cashkeeperweb/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cashkeeperweb::Application.config.secret_token = '5016615ccbd143d1b94b59317c05f0b0e457ab7ec573bc65f41f82786967ccf3b28948899e993651b577b0db5eb02376ae68475ab20674d900d0a622f38830ec'
